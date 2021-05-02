18f20309614a97250429e8e314960f4a5ca46e19
khalea <khaleaberry@gmail.com>
2021-05-02 15:00:41 -0700

    Adding other files

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+// import 'tailwindcss/tailwind.css' // This will remove default styles when uncommented
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
